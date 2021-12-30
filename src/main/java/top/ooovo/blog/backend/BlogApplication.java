93019d513df674a60fbd39a4c83e310cf9a4037a
QAQ <2436781725@qq.com>
2021-12-30 11:50:17 +0800

    初始化项目

---
+++ src/main/java/top/ooovo/blog/backend/BlogApplication.java
@@ -0,0 +1,17 @@
+package top.ooovo.blog.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *  博客后端启动类
+ * @author QAQ
+ * @date 2021/12/30
+ */
+
+@SpringBootApplication
+public class BlogApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BlogApplication.class, args);
+    }
+}
